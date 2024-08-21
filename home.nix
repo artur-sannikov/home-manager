e5dcc118f8bd277738ccd3203eb22b32ec352d0a
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-08-21 21:59:28 +0300

    Add: git

--- home.nix
+++ home.nix
@@ -84,6 +84,21 @@
       enable = true;
     };
 
+    git = {
+      enable = true;
+      userName = "Artur Sannikov";
+      userEmail = "40318410+artur-sannikov@users.noreply.github.com";
+      extraConfig = {
+        url = {
+          "git@github.com:" = {
+            insteadOf = "https://github.com/";
+          };
+        };
+        user.signingKey = "/home/artur/.ssh/git-sign.pub";
+        commit.gpgsign = "true";
+      };
+    };
+
     zsh = {
       enable = true;
       history = {
