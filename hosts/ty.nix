de8cbc982e7dee51dc3786d41dad0156fb0905c0
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-08-24 09:52:54 +0300

    Add: more Flatpaks

--- hosts/ty.nix
+++ hosts/ty.nix
@@ -134,6 +134,7 @@
       update.onActivation = true;
       packages = [
         "org.videolan.VLC"
+        "org.kde.okular"
         "org.zotero.Zotero"
         { appId = "us.zoom.Zoom"; origin = "flathub";  commit = "b9505f108b5f9acb2bbad83ac66f97b42bc6a75b9c28ed7b75dec1040e013305"; } # Sharing broken on Plasma for newer version
       ];
