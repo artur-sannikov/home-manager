b3f156f08cccb86b287ec5ecfaee89e4bd22e1dd
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-08-24 12:49:10 +0300

    change: switch to unstable branch

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
   description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Declarative Nix Flatpaks
