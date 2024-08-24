254ecde14eb14ddf2eab9d101788b12214403a0f
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-08-24 12:34:17 +0300

    add: starship settings

--- hosts/common/cli/starship.nix
+++ hosts/common/cli/starship.nix
@@ -3,5 +3,20 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+
+    settings = {
+      username = {
+        disabled = false;
+        show_always = true;
+        style_user = "green bold";
+      };
+      character = {
+        success_symbol = "[↳](bold green)";
+        error_symbol = "[✗](bold red)";
+      };
+      directory = {
+        read_only = "";
+      };
+    };
   };
 }
