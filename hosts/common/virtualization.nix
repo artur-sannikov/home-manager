099f63d53b40c6e948b89dff0dcb02df301ac722
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-08-26 22:56:50 +0300

    change: modify virtualization

--- hosts/common/virtualization.nix
+++ hosts/common/virtualization.nix
@@ -1,16 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # Various packages related to virtualization, compatability and sandboxing
   home.packages = with pkgs; [
-    # Virtual Machines and wine
     libvirt
     virt-manager
     qemu
   ];
-
-  home.file.".config/libvirt/qemu.conf".text = ''
-    nvram = ["/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd"]
-  '';
-
 }
