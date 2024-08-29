68b3d5dd60b92170ca386979357a77ec1a678049
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-08-29 17:08:45 +0300

    add: direnv zsh integration

--- hosts/common/cli/direnv.nix
+++ hosts/common/cli/direnv.nix
@@ -1,4 +1,7 @@
 {
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableZshIntegration = true;
+  };
 }
